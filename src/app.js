5b9a557256a6e797bcdb657b0a18b49ca36b8973
talha-hashmi141 <talha.hashmi@wamolabs.com>
2025-08-20 19:59:24 +0500

    final 2

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import { logger } from "./utils/logger.js";
 import authRoutes from "./routes/authRoutes.js";
 import jobRoutes from "./routes/jobRoutes.js";
 import { errorHandler } from "./middleware/errorHandler.js";
-import { swaggerUiMiddleware, swaggerUiSetup, swaggerSpec } from "./config/swagger.js";
+import { swaggerSpec } from "./config/swagger.js";
 
 
 const app = express();
@@ -12,9 +12,34 @@ app.use(express.json());
 app.use(logger);
 
 // Swagger
-app.use("/api-docs", swaggerUiMiddleware, swaggerUiSetup);
 app.get("/swagger.json", (req, res) => {
   res.json(swaggerSpec);
+});
+app.get("/api-docs", (req, res) => {
+  res.setHeader("Content-Type", "text/html");
+  res.send(`<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>API Docs</title>
+      <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css" />
+      <style>html, body { margin: 0; padding: 0; } #swagger-ui { min-height: 100vh; }</style>
+    </head>
+    <body>
+      <div id="swagger-ui"></div>
+      <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+      <script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-standalone-preset.js"></script>
+      <script>
+        window.ui = SwaggerUIBundle({
+          url: '/swagger.json',
+          dom_id: '#swagger-ui',
+          presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+          layout: 'StandaloneLayout'
+        });
+      </script>
+    </body>
+  </html>`);
 });
 
 
